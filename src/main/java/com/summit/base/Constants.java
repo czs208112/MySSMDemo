822542ee22b87d4e7a4053fe286e2e1cb3c3f1cb
czs208112 <czs208112@gmail.com>
2018-01-09 16:36:23 +0800

    增加自定义错误代码

--- src/main/java/com/summit/base/Constants.java
+++ src/main/java/com/summit/base/Constants.java
@@ -10,4 +10,13 @@ public class Constants {
      * 验证码认证失败后存储到的属性对应的value值
      */
     public static final String FAILURE_VALUE_ATTRIBUTE = "validateCodeError";
+
+    /********************自定义错误代码开始****************/
+    public static final long ERROR_CODE_UNKNOWN = 9999; //未知错误
+    public static final long ERROR_CODE_VALIDATECODE_ERROR = 1000; //验证码错误
+    public static final long ERROR_CODE_ACCOUNT_NOT_EXIST = 1010; //账号不存在
+    public static final long ERROR_CODE_ACCOUNT_OR_PASSWORD_ERROR = 1011; //账号或者密码错误
+
+    /********************自定义错误代码开始****************/
+
 }
