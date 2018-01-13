30b5ed75374c13730de294154f10c2a323afed85
czs208112 <czs208112@gmail.com>
2018-01-13 12:17:23 +0800

    处理session,实现一个用户同一时刻只能在一个地方登录

--- src/main/java/com/summit/framework/shiro/MyFormAuthenticationFilter.java
+++ src/main/java/com/summit/framework/shiro/MyFormAuthenticationFilter.java
@@ -1,16 +1,23 @@
 package com.summit.framework.shiro;
 
 import com.summit.base.Constants;
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.apache.shiro.web.util.WebUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
 
 /**
  * 回调方法返回布尔类型，通常表示流程是否继续往下走。
@@ -65,10 +72,25 @@ public class MyFormAuthenticationFilter extends FormAuthenticationFilter {
      */
     @Override
     protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response) throws Exception {
-//        WebUtils.issueRedirect(request, response, getSuccessUrl());
+
+//        WebUtils.issueRedirect(request, response, getSuccessUrl());//登陆成功后直接跳转到配置的successUrl
 //        return false;
 
-//        return super.onLoginSuccess(token, subject, request, response);
+//        return super.onLoginSuccess(token, subject, request, response);//登陆成功后走默认流程
+
+        //处理session,实现一个用户同一时刻只能在一个地方登录
+        DefaultWebSecurityManager securityManager = (DefaultWebSecurityManager) SecurityUtils.getSecurityManager();
+        DefaultWebSessionManager sessionManager = (DefaultWebSessionManager) securityManager.getSessionManager();
+        Collection<Session> sessions = sessionManager.getSessionDAO().getActiveSessions();//获取当前已登录的用户session列表
+        for (Session session : sessions) {
+            //清除该用户以前登录时保存的session
+            String userName = request.getParameter(FormAuthenticationFilter.DEFAULT_USERNAME_PARAM);
+            HttpServletRequest httpServletRequest = WebUtils.toHttp(request);
+            String sessionId = httpServletRequest.getSession().getId();
+            if (userName.equals(String.valueOf(session.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY))) && !sessionId.equals(session.getId())) {
+                sessionManager.getSessionDAO().delete(session);
+            }
+        }
 
         return true; //所有登陆成功后的转向交给Controller来解决,本来shiro是可以跳转的，但是由于登陆采用ajax请求，无法直接跳转页面。
     }
